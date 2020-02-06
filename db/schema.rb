87a74ae2631c1844a0a2990758ce482b4adf5b26
Trystan Hendricks <dolkhound@gmx.com>
2020-02-05 23:30:07 -0500

    migration for images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_030043) do
+ActiveRecord::Schema.define(version: 2020_02_06_042614) do
 
   create_table "listings", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2020_02_06_030043) do
     t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
