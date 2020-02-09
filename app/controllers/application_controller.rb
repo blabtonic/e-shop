687a38af0c1f35bb2e7144d89b4de88d65cb5a5a
Trystan Hendricks <dolkhound@gmx.com>
2020-02-08 20:58:02 -0500

    setting up devise parameter for validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising exception
+    protect_from_forgery with: :exception
+
+    before_filter :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.for(:sign_up) << :name
+        devise_parameter_sanitizer.for(:account_update) << :name
+    end
 end
