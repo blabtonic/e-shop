d3ffa6cba7be7bd59510c862776bf55038f37ca7
Trystan Hendricks <dolkhound@gmx.com>
2020-02-12 20:56:32 -0500

    validation added for image

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,5 @@ class Listing < ApplicationRecord
 
     validates :name, :description, :price, presence: true
     validates :price, numericality: { greater_than: 0 }
+    validates_attachment_presence :image
 end
